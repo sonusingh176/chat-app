75f7cfd2412a0313358c8a288699467709edfc77
sonusingh176 <sonusingh82601@gmail.com>
2025-08-22 00:32:08 +0530

    create message schema

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ const express =require('express');
 const app = express();
 const authRouter = require('./controller/authController');
 const userRouter = require('./controller/userController');
+const chatRouter = require('./controller/chatController');
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
 
 app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
+app.use('/api/chat',chatRouter);
 
 
 
