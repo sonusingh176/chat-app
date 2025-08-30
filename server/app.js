e9f0d6dab8f9e0cf02597e43de27a42699c0dc43
sonusingh176 <sonusingh82601@gmail.com>
2025-08-30 17:23:38 +0530

    message api

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const authRouter = require('./controller/authController');
 const userRouter = require('./controller/userController');
 const chatRouter = require('./controller/chatController');
+const messageRouter = require('./controller/messageController');
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
@@ -10,6 +11,8 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
 app.use('/api/chat',chatRouter);
+app.use('/api/message',messageRouter);
+
 
 
 
