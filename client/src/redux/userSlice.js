b53ad05dc7158aadb093d25cd6503afff032134f
sonusingh176 <sonusingh82601@gmail.com>
2025-10-05 14:52:41 +0530

    get all user from DB

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -1,17 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { all } from "axios";
 
 const userSlice =createSlice({
 
     name: 'user',
     initialState:{
         user: null, // initially koi user logged-in nahi
-      },
+        allUsers:[],
+    },
     reducers:{
         setUser:(state, action)=>{state.user=action.payload;}, // action.payload mein user ka data aayega
+        setAllUsers:(state, action)=>{state.allUsers=action.payload;}, // action.payload mein all users ka data aayega
     }
 
 
 });
 
-export const {setUser} = userSlice.actions;
+export const {setUser,setAllUsers} = userSlice.actions;
 export default userSlice.reducer;
